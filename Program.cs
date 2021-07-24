8a5e4825b60defac2036da49669fe39c679ac883
Harsh Tiwari <77021574+harshtiwarii@users.noreply.github.com>
2021-07-24 18:50:10 +0530

    Initial commit

---
+++ Program.cs
@@ -0,0 +1,30 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Logging;
+
+namespace Catalog
+{
+    public class NewBaseType
+    {
+        public static IHostBuilder CreateHostBuilder(string[] args) =>
+            Host.CreateDefaultBuilder(args)
+                .ConfigureWebHostDefaults(webBuilder =>
+                {
+                    webBuilder.UseStartup<Startup>();
+                });
+        public static void Main(string[] args) => CreateHostBuilder(args).Build().Run();
+    }
+
+    // public class NewBaseType
+    // {
+    // }
+
+    // public class Program : NewBaseType, NewBaseType
+    // {
+    // }
+}
